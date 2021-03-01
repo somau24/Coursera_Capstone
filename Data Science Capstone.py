fa84c5d2e0416e62d22a2fad69375c3a25e50557
somau24 <74869406+somau24@users.noreply.github.com>
2021-03-01 14:01:57 +0800

    Add files via upload

---
+++ Data Science Capstone.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1>this notebook will be mainly used for the capstone project.</h1>
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+print('Hello Capstone Project Course!')
+
